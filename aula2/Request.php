d3c5c4c7b7d35a67c077cd13aa9bed5f28dd18c2
maxstur <futuromaxs@gmail.com>
2016-08-09 22:07:11 -0300

    Repositorio

---
+++ aula2/Request.php
@@ -0,0 +1,102 @@
+<?php
+
+class Request
+{
+	private $method;
+	private $protocol;
+	private $ip;
+	private $resource;
+	private $params;
+
+
+	public function __construct($method,$protocol,$ip,$resource,$params)
+	{
+		
+		$this->set_method($method);
+		$this->set_protocol($protocol);
+		$this->set_ip($ip);
+		$this->set_resource($resource);
+		$this->set_params($params);
+	}
+
+	public function toString()
+	{
+	
+	
+	
+	
+	}
+
+	
+	
+	
+	
+
+	private function get_method()
+	{
+		return $this->method;
+	}
+	
+	private function get_protocol()
+	{
+		return $this->protocol;
+	}
+	
+	private function get_ip()
+	{
+		return $this->ip = $ip;
+	}
+	
+	private function get_resource()
+	{
+		return $this->params;
+	}
+
+	private function get_params()
+	{
+		return $this->params;
+	}
+
+	private function set_method($method)
+	{
+		$this->method = $method;
+	}
+	
+	private function set_protocol($protocol)
+	{
+		$this->protocol = $protocol;
+	}
+	
+	private function set_ip($ip)
+	{
+		$this->ip = $ip;
+	}
+	
+	private function set_resource($resource)
+	{
+		$this->params = $params;
+	}
+
+	private function set_params($params)
+	{
+		$this->params = $params;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
